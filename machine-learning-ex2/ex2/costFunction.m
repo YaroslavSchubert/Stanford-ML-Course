a3134301e8e554b69a832decc79b0d6bbbbf6e96
Yaroslav Schubert <YaroslavShubert@gmail.com>
2016-12-23 10:49:38 +0200

    correct sigm, grad, cost

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,13 +20,20 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X*theta);
+J = (-y'*log(h) - (1 - y)'*log(1 - h))/m;
 
-
-
-
-
-
+err = h - y;
+grad = (X' * err)/m;
 
 % =============================================================
 
 end
+
+% h = sigmoid(X*theta);
+
+% J = -1/m * (y*log(h) + (1-y)*log(1-h));  
+
+% err = h - y;
+% grad = (1/m)*(X' * err);
+
